9bb302234726c16ac6f35ad31a98e2a7955ba67f
unknown <nikkalyan52@gmail.com>
2024-07-31 12:25:40 +0530

    compiling all function

--- day2/index.js
+++ day2/index.js
@@ -1,11 +1,70 @@
 
-const App =() =>{
+
+
+
+const Navbar =(user) =>{
+    console.log(user)
     return (
+        
+
+       
         <div>
-            <h2>welcome to my app</h2>
+            <h2>navbar1{user}</h2>
         </div>
     )
 }
 
+const Content =(user) =>{
+    console.log(user)
+    return (
+        
+
+       
+        <div>
+            <h2> content{user}</h2>
+        </div>
+    )
+}
+
+const Footer =(user) =>{
+    console.log(user)
+    return (
+        
+
+       
+        <div>
+            <h2>footer{user}</h2>
+        </div>
+    )
+}
+
+// camplie the all function here 
+const App =() =>{
+    let user = "Chota Bheem"
+
+    return (
+        <>
+        
+        {/* {Navbar()}  // this is also one way to call 
+        {Content()}
+        {Footer(user)} */}
+        <Navbar />
+        <Content />
+        <Footer />
+
+        </>
+    )
+}
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
+// root.render(<Navbar/>)  // not work like this 
+// root.render(<Content />)
+// root.render(<Footer/>)
+
+//
