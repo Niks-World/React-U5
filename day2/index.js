197a9c6b8980cf830df5f6acc6bd6dd32e30ec25
unknown <nikkalyan52@gmail.com>
2024-07-31 12:02:35 +0530

    compnent

---
+++ day2/index.js
@@ -0,0 +1,11 @@
+
+const App =() =>{
+    return (
+        <div>
+            <h2>welcome to my app</h2>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
