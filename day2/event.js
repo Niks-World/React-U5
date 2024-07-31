2091ed5a3f5651158cbf929f51c2517eef16d9ab
unknown <nikkalyan52@gmail.com>
2024-07-31 14:00:10 +0530

    light and dark mood

--- day2/event.js
+++ day2/event.js
@@ -2,16 +2,16 @@
 const Counter = () =>{
     // let count = 0;
 
-    let [count, setCount] = React.useState(0)
+    let [count, setCount] = React.useState(0) // we give default value 0 can change to any num 99
     function handelClick(num){
 
        setCount((count)=>  count + num)
         // console.log(count)
-
     }
+    // You dont need function you can use inline function 
     return (
-        <div>
-            <button onClick={()=>handelClick(-1)}>Decremnet</button>
+        <div>    
+            <button onClick={()=>setCount((count)=>  count - 1)}>Decremnet</button>
             <h2>{count}</h2>
             <button onClick={()=>handelClick(1)}>Incerment</button>
         </div>
---
+++ day2/theme.js
@@ -0,0 +1,35 @@
+
+
+
+
+const DarkMode = ()=>{
+    return (
+        <div style={{backgroundColor:"black" , padding:"40px" , color:"white" }}>
+            <h2>Hello world</h2>
+        </div>
+    )
+  }
+  
+  const LightMode = ()=>{
+    return (
+        <div style={{backgroundColor:"rgb(163, 163, 205)" , padding:"40px" , color:"midnightblue" }}>
+            <h2>Hello world</h2>
+        </div>
+    )
+  }
+  
+  const App = ()=>{
+    let [toggle, setToggle] = React.useState(true)
+    return (
+        <>
+        <button onClick={()=>setToggle(!toggle)}>Toggle Themes</button>
+        
+            {
+                toggle ? <DarkMode/>: <LightMode/>
+            }
+        </>
+    )
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"))
+  root.render(<App/>)
