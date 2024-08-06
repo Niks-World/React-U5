c0364a90bf25016ad387644cfbb6f04b74984f58
unknown <nikkalyan52@gmail.com>
2024-08-06 13:34:57 +0530

    pagenation

--- Sprint 2/vite-project D1/src/componet/ShowData.jsx
+++ Sprint 2/vite-project D1/src/componet/ShowData.jsx
@@ -3,15 +3,16 @@ import { useState , useEffect} from "react"
 
 export const ShowData =() =>{
 
-    let URL = `https://jsonplaceholder.typicode.com/posts`
+    let URL = `https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10`
 
     let [data , setData] = useState([])
+    let [page, setpage] = useState(1)
 
     const getData = () =>{
-        axios.get(URL)
+        axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`)
         .then((res)=>{
             setData(res.data)
-            console.log(res.data)
+            // console.log(res.data)
         })
         .catch((error)=> console.log(error))
     }
@@ -20,12 +21,24 @@ export const ShowData =() =>{
 
     useEffect(()=>{
         getData()
-    }, [])
+    }, [page])
     
     return (
         <>
-            <div>
-
+        <div>
+            <button onClick={()=>setpage(page-1)} disabled={page == 1}>prev</button>
+            <h2>{page}</h2>
+            <button onClick={()=>setpage(page+1)}>next</button>
+        </div>
+            <div>{
+                 data.map((ele) =>{
+                    return (
+                        <div key={ele.id}>
+                            <p><b>{ele.id}. Title :</b> {ele.title}</p>
+                        </div>
+                    )
+                 })
+            }
             </div>
         </>
     )
