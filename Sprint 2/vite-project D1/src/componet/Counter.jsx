d58a01073dad3c72b9331ad024802d08a7065e2c
unknown <nikkalyan52@gmail.com>
2024-08-06 13:54:25 +0530

    useEffect added

--- Sprint 2/vite-project D1/src/componet/Counter.jsx
+++ Sprint 2/vite-project D1/src/componet/Counter.jsx
@@ -1,14 +1,24 @@
-export const Counter = ({count , setCount}) => {
-    return (
-        <>
+import { useEffect } from "react"
 
+export const Counter = ({count , setCount}) => {
+   
+        
+        useEffect(() =>{
+            let intervalID = setInterval(()=>{
+                console.log(count)
+            }, 1000)
+
+            return ()=> clearInterval(intervalID); // cleanup function 
+        }, [count])
+        
+        return (
         <div>
          <button onClick={()=>setCount(count -1)}>DEc</button>
          <input value={count} />
          <button onClick={()=>setCount(count+1)}>Inc</button>
  
         </div>
-     </>
+    
     )
 }
 
